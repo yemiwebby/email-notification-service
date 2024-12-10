4aec6fd01aa0909011657491a41dcc5d9272910c
yemiwebby <yemiwebby@gmail.com>
2024-12-10 07:42:52 +0000

    use concurrency

--- internal/handler/email.go
+++ internal/handler/email.go
@@ -14,29 +14,6 @@ func RegisterEmailRoutes(r *mux.Router) {
 	r.HandleFunc("/send", SendEmail).Methods("POST")
 }
 
-// func SendEmail(w http.ResponseWriter, r *http.Request) {
-// 	start := time.Now()
-
-// 	var req service.EmailRequest
-// 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-// 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	go func() {
-// 		err := service.SendEmail(req)
-// 		if err != nil {
-// 			http.Error(w, "Failed to send email", http.StatusInternalServerError)
-// 			return
-// 		}
-// 	}()
-
-// 	log.Printf("Email processing completed in %v\n", time.Since(start))
-// 	w.WriteHeader(http.StatusOK)
-// 	json.NewEncoder(w).Encode(map[string]string{"message": "Email sent successfully"})
-// }
-
-
 func SendEmail(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 
@@ -46,15 +23,38 @@ func SendEmail(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Synchronous email sending (without concurrency)
-	err := service.SendEmail(req)
-	if err != nil {
-		log.Printf("Failed to send email: %v\n", err)
-		http.Error(w, "Failed to send email", http.StatusInternalServerError)
-		return
-	}
+	go func() {
+		err := service.SendEmail(req)
+		if err != nil {
+			http.Error(w, "Failed to send email", http.StatusInternalServerError)
+			return
+		}
+	}()
 
 	log.Printf("Email processing completed in %v\n", time.Since(start))
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{"message": "Email sent successfully"})
 }
+
+
+// func SendEmail(w http.ResponseWriter, r *http.Request) {
+// 	start := time.Now()
+
+// 	var req service.EmailRequest
+// 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+// 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	// Synchronous email sending (without concurrency)
+// 	err := service.SendEmail(req)
+// 	if err != nil {
+// 		log.Printf("Failed to send email: %v\n", err)
+// 		http.Error(w, "Failed to send email", http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	log.Printf("Email processing completed in %v\n", time.Since(start))
+// 	w.WriteHeader(http.StatusOK)
+// 	json.NewEncoder(w).Encode(map[string]string{"message": "Email sent successfully"})
+// }
